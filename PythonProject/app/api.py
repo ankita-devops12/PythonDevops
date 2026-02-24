89471cbe5a25b6f3cbb80e4b5422df68040487fb
ankita-devops12 <ankita.bgholap12@gmail.com>
2026-02-24 14:49:29 +0530

    Add files via upload
    Python Project

---
+++ PythonProject/app/api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI # Importing FastAPI Class
+from routers import metrics
+
+app = FastAPI(
+    title="Internal DevOps Utilities API",
+    description="This is an Internal API Utitlities App for Monitoring metrics, AWS Usage, Log Analysis, etc",
+    version="1.1.0",
+    doc_url="/docs",
+    redoc_url="/redoc"
+)
+
+@app.get("/")
+def hello():
+    """
+    This is a Hello API , just for testing
+    """
+    return {"message":"Hello Dosto, This is DevOps Utilites API"}
+
+app.include_router(metrics.router)
